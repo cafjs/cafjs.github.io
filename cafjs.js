237a53cfcb3381ef39cc1c699e4ef9d974ee2f96
Antonio Lain <antlai@cafjs.com>
2021-04-29 13:55:35 -0700

    Add new site version
    Signed-off-by: Antonio Lain <antlai@cafjs.com>

--- cafjs.js
+++
@@ -1,9 +0,0 @@
-/*
- * From bootstrap offcanvas example. MIT license, copyright 2016 Twitter
-*/
-
-$(document).ready(function () {
-  $('[data-toggle="offcanvas"]').click(function () {
-    $('.row-offcanvas').toggleClass('active')
-  });
-});
