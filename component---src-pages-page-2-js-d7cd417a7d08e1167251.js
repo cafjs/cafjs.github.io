fd96cbf25decde14cb1258ed1395489ca7618dc8
Antonio Lain <antlai@cafjs.com>
2021-11-19 19:06:40 -0800

    Add video links
    Signed-off-by: Antonio Lain <antlai@cafjs.com>

--- component---src-pages-page-2-js-d7cd417a7d08e1167251.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[15],{p5nM:function(e,a,t){"use strict";t.r(a);var n=t("q1tI"),o=t.n(n),c=t("Wbzz"),l=t("Bl7J"),p=t("vrFN");a.default=function(){return o.a.createElement(l.a,{pageInfo:{pageName:"page-2"}},o.a.createElement(p.a,{title:"Page two"}),o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(c.a,{to:"/"},"Go back to the homepage"))}}}]);
-//# sourceMappingURL=component---src-pages-page-2-js-d7cd417a7d08e1167251.js.map
